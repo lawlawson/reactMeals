75e71fa4bf46cd464f97e6db4bf69179cd32734a
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2021-12-08 23:58:21 +0000

    Added form fields

---
+++ src/components/Cart/Checkout.js
@@ -0,0 +1,26 @@
+import classes from './Checkout.module.css';
+
+const Checkout = () => {
+  return (
+    <form>
+      <div className={classes.control}>
+        <label htmlFor='name'>Your name</label>
+        <input type='text' id='name' />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor='street'>Street</label>
+        <input type='text' id='street' />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor='postal'>Postal Code</label>
+        <input type='text' id='postal' />
+      </div>
+      <div className={classes.control}>
+        <label>City</label>
+        <input type='text' id='city' />
+      </div>
+    </form>
+  );
+};
+
+export default Checkout;
